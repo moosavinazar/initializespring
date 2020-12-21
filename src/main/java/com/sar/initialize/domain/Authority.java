79e68d1e9585ae0b140965c36cd05f99145ac52d
Moosavi <s.ha.mousavi.nazar@gmail.com>
2020-12-21 08:19:29 +0330

    add Role entity for define project access roles dynamic

---
+++ src/main/java/com/sar/initialize/domain/Authority.java
@@ -0,0 +1,17 @@
+package com.sar.initialize.domain;
+
+public enum Authority {
+
+    OP_VIEW_USER_LIST,
+    OP_VIEW_USER,
+    OP_CREATE_USER,
+    OP_UPDATE_USER,
+    OP_DELETE_USER,
+
+    OP_VIEW_ROLE_LIST,
+    OP_VIEW_ROLE,
+    OP_CREATE_ROLE,
+    OP_UPDATE_ROLE,
+    OP_DELETE_ROLE,
+    ;
+}
---
+++ src/main/java/com/sar/initialize/domain/Role.java
@@ -0,0 +1,53 @@
+package com.sar.initialize.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table("sar_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @ElementCollection(targetClass = Authority.class)
+    //@Enumerated(value = EnumType.STRING)
+    private Set<Authority> authorities = new HashSet<>();
+
+    public Role() {
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authority> authorities) {
+        this.authorities = authorities;
+    }
+}
