710608cea608f39fae63246f35d993fba4ce799c
Moosavi <s.ha.mousavi.nazar@gmail.com>
2020-12-21 08:09:54 +0330

    initialize Spring Boot project

---
+++ src/test/java/com/sar/initialize/InitializeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sar.initialize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
